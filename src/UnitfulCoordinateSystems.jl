fa145266e1949138b52ece007db68632e3e801e4
Michael Ingold <mike.ingold@gmail.com>
2023-04-02 18:07:47 -0400

    Fix recursive getproperty bug

--- src/UnitfulCoordinateSystems.jl
+++ src/UnitfulCoordinateSystems.jl
@@ -51,21 +51,100 @@ module UnitfulCoordinateSystems
     #                        ACCESSOR FUNCTIONS
     ###########################################################################
 
-    function Base.getproperty(coord::AbstractCoordinate, sym::Symbol)
-        if sym == :x
+    function Base.getproperty(coord::CoordinatePolar, sym::Symbol)
+        if (sym == :rho) || (sym == :phi)
+            return getfield(coord, sym)
+        elseif sym == :x
             return x(coord)
         elseif sym == :y
             return y(coord)
         elseif sym == :z
             return z(coord)
-        elseif (sym == :ρ) || (sym == :rho)
-            return ρ(coord)
-        elseif (sym == :φ) || (sym == :ϕ) || (sym == :phi)
-            return φ(coord)
-        elseif (sym == :θ) || (sym == :ϑ) || (sym == :theta)
-            return θ(coord)
-        else
-            # Fallback
+        elseif sym == :r
+            return r(coord)
+        elseif sym == :ρ
+            return rho(coord)
+        elseif (sym == :φ) || (sym == :ϕ)
+            return phi(coord)
+        elseif (sym == :θ) || (sym == :ϑ)
+            return theta(coord)
+        else # Fallback
+            return getfield(coord, sym)
+        end
+    end
+
+    function Base.getproperty(coord::CoordinateRectangular, sym::Symbol)
+        if (sym == :x) || (sym == :y)
+            getfield(coord, sym)
+        elseif sym == :z
+            return z(coord)
+        elseif sym == :r
+            return r(coord)
+        elseif (sym == :rho) || (sym == :ρ)
+            return rho(coord)
+        elseif (sym == :phi) || (sym == :φ) || (sym == :ϕ)
+            return phi(coord)
+        elseif (sym == :theta) || (sym == :θ) || (sym == :ϑ)
+            return theta(coord)
+        else # Fallback
+            return getfield(coord, sym)
+        end
+    end
+    
+    function Base.getproperty(coord::CoordinateCartesian, sym::Symbol)
+        if (sym == :x) || (sym == :y) || (sym == :z)
+            getfield(coord, sym)
+        elseif sym == :r
+            return r(coord)
+        elseif (sym == :rho) || (sym == :ρ)
+            return rho(coord)
+        elseif (sym == :phi) || (sym == :φ) || (sym == :ϕ)
+            return phi(coord)
+        elseif (sym == :theta) || (sym == :θ) || (sym == :ϑ)
+            return theta(coord)
+        else # Fallback
+            return getfield(coord, sym)
+        end
+    end
+
+    function Base.getproperty(coord::CoordinateCylindrical, sym::Symbol)
+        if (sym == :rho) || (sym == :phi) || (sym == :z)
+            getfield(coord, sym)
+        elseif sym == :x
+            return x(coord)
+        elseif sym == :y
+            return y(coord)
+        elseif sym == :r
+            return r(coord)
+        elseif sym == :ρ
+            return rho(coord)
+        elseif (sym == :φ) || (sym == :ϕ)
+            return phi(coord)
+        elseif (sym == :theta) || (sym == :θ) || (sym == :ϑ)
+            return theta(coord)
+        else # Fallback
+            return getfield(coord, sym)
+        end
+    end
+
+    function Base.getproperty(coord::CoordinateSpherical, sym::Symbol)
+        if (sym == :r) || (sym == :theta) || (sym == :phi)
+            getfield(coord, sym)
+        elseif sym == :x
+            return x(coord)
+        elseif sym == :y
+            return y(coord)
+        elseif sym == :z
+            return z(coord)
+        elseif sym == :r
+            return r(coord)
+        elseif (sym == :rho) || (sym == :ρ)
+            return rho(coord)
+        elseif (sym == :φ) || (sym == :ϕ)
+            return phi(coord)
+        elseif (sym == :θ) || (sym == :ϑ)
+            return theta(coord)
+        else # Fallback
             return getfield(coord, sym)
         end
     end
