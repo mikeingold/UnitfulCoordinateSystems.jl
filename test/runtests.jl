2dd889551dad78cd692bc12c21b21a4f8856f95e
Michael Ingold <mike.ingold@gmail.com>
2023-04-02 19:00:23 -0400

    Add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -60,3 +60,43 @@ end
 	@test UnitfulCoordinateSystems.θ(coord)     ≈ acosd(3.0/sqrt(14.0)) * u"°"
 	@test UnitfulCoordinateSystems.ϑ(coord)     ≈ acosd(3.0/sqrt(14.0)) * u"°"
 end
+
+@testset "Component Functions on CoordinateCylindrical" begin
+	coord = CoordinateCylindrical(1.0u"m", 45.0u"°", 3.0u"m")
+
+	@test UnitfulCoordinateSystems.x(coord) ≈ (sqrt(2.0) / 2.0) * u"m"
+	@test UnitfulCoordinateSystems.y(coord) ≈ (sqrt(2.0) / 2.0) * u"m"
+	@test UnitfulCoordinateSystems.z(coord) ≈ 3.0u"m"
+	@test UnitfulCoordinateSystems.r(coord) ≈ sqrt(10.0)*u"m"
+
+	@test UnitfulCoordinateSystems.ρ(coord)   ≈ 1.0u"m"
+	@test UnitfulCoordinateSystems.rho(coord) ≈ 1.0u"m"
+	
+	@test UnitfulCoordinateSystems.phi(coord) ≈ 45.0u"°"
+	@test UnitfulCoordinateSystems.φ(coord)   ≈ 45.0u"°"
+	@test UnitfulCoordinateSystems.ϕ(coord)   ≈ 45.0u"°"
+	
+	@test UnitfulCoordinateSystems.theta(coord) ≈ acosd(3.0) * u"°"
+	@test UnitfulCoordinateSystems.θ(coord)     ≈ acosd(3.0) * u"°"
+	@test UnitfulCoordinateSystems.ϑ(coord)     ≈ acosd(3.0) * u"°"
+end
+
+@testset "Component Functions on CoordinateSpherical" begin
+	coord = CoordinateSpherical(1.0u"m", 45.0u"°", 45.0u"°")
+
+	@test UnitfulCoordinateSystems.x(coord) ≈ 0.5u"m"
+	@test UnitfulCoordinateSystems.y(coord) ≈ 0.5u"m"
+	@test UnitfulCoordinateSystems.z(coord) ≈ (sqrt(2.0) / 2.0) * u"m"
+	@test UnitfulCoordinateSystems.r(coord) ≈ 1.0u"m"
+
+	@test UnitfulCoordinateSystems.ρ(coord)   ≈ (sqrt(2.0) / 2.0) * u"m"
+	@test UnitfulCoordinateSystems.rho(coord) ≈ (sqrt(2.0) / 2.0) * u"m"
+	
+	@test UnitfulCoordinateSystems.phi(coord) ≈ 45.0u"°"
+	@test UnitfulCoordinateSystems.φ(coord)   ≈ 45.0u"°"
+	@test UnitfulCoordinateSystems.ϕ(coord)   ≈ 45.0u"°"
+	
+	@test UnitfulCoordinateSystems.theta(coord) ≈ 45.0u"°"
+	@test UnitfulCoordinateSystems.θ(coord)     ≈ 45.0u"°"
+	@test UnitfulCoordinateSystems.ϑ(coord)     ≈ 45.0u"°"
+end
